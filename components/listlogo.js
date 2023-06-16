5a3f036e2914d4c7c4b366408edd1246896df062
RikyAdiPutra <rikyadiputra76@gmail.com>
2023-06-16 20:11:09 +0700

    add slider

--- components/listlogo.js
+++ components/listlogo.js
@@ -5,8 +5,13 @@
 // 2. Isi Di bagian elemennya (Attribut) = (props. ...) / {props.(width/height)}
 // Props == Distactoring Data
 
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 function Itemlogo(props) {
     return (
@@ -16,18 +21,55 @@ function Itemlogo(props) {
     )
 }
 
+
+
 export default function Listlogo() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]    
+  }
     return (
-        <div className='py-[20px] flex items-center gap-5 justify-between'>
-            <div className="container flex items-center justify-between">
-                <Itemlogo image="/logo/logo1.png" width={119} height={71}/>
-                <Itemlogo image="/logo/logo2.png" width={151} height={53}/>
-                <Itemlogo image="/logo/logo3.png" width={177} height={63}/>
-                <Itemlogo image="/logo/logo4.png" width={236} height={71}/>
-                <Itemlogo image="/logo/logo5.png" width={128} height={71}/>
-                <Itemlogo image="/logo/logo6.png" width={167} height={64}/>
-            </div>
-        </div>
+      <div className='bg-yellowcustom py-[20px]  items-center gap-5 justify-between ym-10'>
+          <div>
+            <Slider {...settings}>
+              <Itemlogo image="/logo/logo1.png" width={119} height={71}/>
+              <Itemlogo image="/logo/logo2.png" width={119} height={71}/>
+              <Itemlogo image="/logo/logo3.png" width={119} height={71}/>
+              <Itemlogo image="/logo/logo4.png" width={119} height={71}/>
+              <Itemlogo image="/logo/logo5.png" width={119} height={71}/>
+              <Itemlogo image="/logo/logo6.png" width={119} height={71}/>
+            </Slider>
+          </div>
+      </div>
     )
-}
+  }
 
